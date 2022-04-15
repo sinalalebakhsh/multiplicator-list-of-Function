74c91d6ac1ffe77936e7eb4c93971051a4cd2430
sina lalebakhsh <93151188+sinalalebakhsh@users.noreply.github.com>
2022-04-15 12:56:03 +0430

    تابع جمع کنندۀ آیتم های لیست در پایتون
    multiplicator list of Function

---
+++ 26- def introduction ver8.py
@@ -0,0 +1,13 @@
+
+def import_os():
+    import os
+    os.system('cls')
+import_os()
+
+list_number = [1, 2, 3, 4, 5, 6, 7,]
+
+def list_multiplicator(listnum):
+    return sum(listnum)
+
+print(list_multiplicator(list_number))
+
